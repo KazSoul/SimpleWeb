63693c01ca55ab9317f71e9bfa4adeffafce1ca1
KazSoul <kazsoul0509@gmail.com>
2021-02-04 11:15:08 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+const myButton = document.querySelector('button');
+const myHeading = document.querySelector('h1');
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc == "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
